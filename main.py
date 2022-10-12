ac127e207b543228a53f60ccd5e83d79de1c9742
sarah castorena <ceanz@yahoo.com>
2022-10-11 19:47:44 -0700

    new commit

--- main.py
+++ main.py
@@ -2,3 +2,12 @@
 # ******************************
 # Make your Code
 # ******************************
+# sarah Castorena
+# SID 2028049
+
+numbers = []
+for i in range(5):
+    numbers.append(int(input('Enter a number: ')))
+avg = sum(numbers)/len(numbers)
+numbers  = [i for i in range(len(numbers)) if numbers[i] > avg]    
+print(*numbers, sep = ' ')
