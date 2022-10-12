12389a4beae7db3c48625b3840079731a4fc0976
sarah castorena <ceanz@yahoo.com>
2022-10-11 20:11:26 -0700

    new commit

--- main.py
+++ main.py
@@ -6,8 +6,12 @@
 # SID 2028049
 
 numbers = []
+num2 = []
 for i in range(5):
-    numbers.append(int(input('Enter a number: ')))
-avg = sum(numbers)/len(numbers)
-numbers  = [i for i in range(len(numbers)) if numbers[i] > avg]    
-print(*numbers, sep = ' ')
+    numbers.append(int(input('Enter a number: ')))   
+avg = int(sum(numbers)/len(numbers))
+for i in range(len(numbers)):
+    if numbers[i] > avg:
+       num2.append(numbers[i])  
+for i in range(len(num2)):
+   print(num2[i], end = ' ')
